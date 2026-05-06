39c3dc3ac75fea539c0b9c296f7eab336ffdf44b
Mauricio Araya <mauricio@MacBook-Pro-de-Mauricio.local>
2026-05-05 18:28:54 -0600

    Cap 05 - 5-7: favorite fruit

---
+++ cap_05/5-7-favorite-fruit.py
@@ -0,0 +1,17 @@
+# Make a list of your favorite fruits, and then write a series of independent if 
+# statements that check for certain fruits in your list.
+# Make a list of your three favorite fruits and call it favorite_fruits.
+# Write five if statements. Each should check whether a certain kind of fruit 
+# is in your list. If the fruit is in your list, the if block should print a 
+# statement, such as You really like bananas!
+favorite_fruits = ['apple', 'banana', 'orange']
+if 'apple' in favorite_fruits:
+    print("You really like apples!")
+if "banana" in favorite_fruits:
+    print("You really like bananas!")
+if "orange" in favorite_fruits:
+    print("You really like oranges")
+if "kiwi" in favorite_fruits:
+    print("You really like kiwis")
+if "mango" in favorite_fruits:
+    print("You really like mangos")
