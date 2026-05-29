d5e9623d7df1e356b0fe4cc0cd5872b738a63bf1
SUB-JEFE BODEGA <subjefe.bodega@favarciacr.local>
2026-05-29 16:33:35 -0600

    cap 06 - ex 6.6: rivers dictionary with keys and values loops

--- cap_06_dictionaries/6-5-rivers.py
+++ cap_06_dictionaries/6-5-rivers.py
@@ -1,7 +1,9 @@
 #  Make a dictionary containing three major rivers and the country each river 
 # runs through. One key-value pair might be 'nile': 'egypt'.
 # Use a loop to print a sentence about each river, such as The Nile runs through 
-# Egypt.
+# Egypt. 
+# Use a loop to print the name of each river included in the dictionary. 
+# Use a loop to print the name of each country included in the dictionary
 
 rivers = {
     'nile': 'egypt',
@@ -10,3 +12,9 @@ rivers = {
     }
 for river, country in rivers.items():
     print(f"The {river.title()} runs through {country.title()}.") 
+
+for river in rivers.keys():
+    print(river.title())
+
+for country in rivers.values():
+    print(country.title())
