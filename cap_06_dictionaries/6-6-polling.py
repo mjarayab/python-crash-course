0e4a7e53168cfc38d1ed28ef7ab3c50b61b16afb
SUB-JEFE BODEGA <subjefe.bodega@favarciacr.local>
2026-05-29 17:03:49 -0600

    cap 06 - ex 6.6: favorite languages poll with conditional

---
+++ cap_06_dictionaries/6-6-polling.py
@@ -0,0 +1,20 @@
+# Use the code in favorite_languages.py (page 96). Make a list of people who 
+# should take the favorite languages poll. Include some names that are already 
+# in the dictionary and some that are not.
+# Loop through the list of people who should take the poll. If they have already
+# taken the poll, print a message thanking them for responding. If they have not 
+# yet taken the poll, print a message inviting them to take the poll.
+
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'rust',
+'phil': 'python',
+}
+
+people = ['mauricio', 'jen', 'ana', 'phil']
+for name in people:
+    if name in favorite_languages:
+        print (f"Thanks {name.title()}! for taking the poll")
+    else:
+        print(f"{name.title()} please take the poll")
